7981caa33afe52c4d570130e694cb3ce78859aa2
misskecupbung <ananda.dwirahmawati313@gmail.com>
2023-05-29 00:42:04 +0700

    add workflow for push to ecr rentcar

--- rentcar/config/config.go
+++ rentcar/config/config.go
@@ -2,7 +2,7 @@ package config
 
 var (
 	Server_Port = "0.0.0.0:8000"
-	Mongo_Host  = "mongodb://mongodb-svc:27017"
+	Mongo_Host  = "mongodb://mongodb-rentcar-svc:27017"
 	// Ex: For local:
 	// Mongo_Host = "mongodb://localhost:27017"
 )
